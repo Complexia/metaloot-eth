99d812489be8a49ac4a70e40f92bac47cc1c41d1
Complexia <complexia701@gmail.com>
2024-11-15 23:42:31 +0700

    init flow app inside metaloot tauri

--- metaloot-tauri/next.config.mjs
+++ metaloot-tauri/next.config.mjs
@@ -1,6 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     output: 'export',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.supabase.co',
+                pathname: '/storage/v1/object/public/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'biz-touch-7unj.shuttle.app',
+                pathname: '/api/**',
+            },
+        ],
+    },
+    async headers() {
+        return [
+          {
+            // Match all routes
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: [
+                  "default-src 'self';",
+                  "connect-src 'self' https://rest-testnet.onflow.org https://biz-touch-7unj.shuttle.app;",
+                  "img-src 'self' https://*.supabase.co;",
+                  "script-src 'self';",
+                  "style-src 'self';",
+                  "frame-src 'self' https://fcl-discovery.onflow.org;", // Allow the Flow discovery URL
+                ].join(' '),
+              },
+            ],
+          },
+        ];
+      },
 };
 
 export default nextConfig;
