3d92426290d56deb98187e478cb6a1e446bd4799
vanha777 <vanha777@icloud.com>
2024-11-17 10:34:13 +0700

    x

--- trophyhunter/next.config.ts
+++ trophyhunter/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: 'export',
+  basePath: '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   async rewrites() {
     return [
       {
@@ -13,3 +23,29 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//       // Warning: This allows production builds to successfully complete even if
+//       // your project has ESLint errors.
+//       ignoreDuringBuilds: true,
+//   },
+//   output: 'export',
+//   images: {
+//       remotePatterns: [
+//           {
+//               protocol: 'https',
+//               hostname: '*.supabase.co',
+//               pathname: '/storage/v1/object/public/**',
+//           },
+//           {
+//               protocol: 'https',
+//               hostname: 'biz-touch-7unj.shuttle.app',
+//               pathname: '/api/**',
+//           },
+//       ],
+//   },
+// };
+
+// export default nextConfig;
