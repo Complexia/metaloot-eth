40b35db1f547b3a9a13658602eba3bd10bdd58f8
Complexia <complexia701@gmail.com>
2024-11-19 22:45:14 +1100

    added links

--- metaloot-landing/app/page.tsx
+++ metaloot-landing/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://ethglobal.com/showcase/metaloot-x5810"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -62,9 +62,9 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          ETHGlobal Demo
         </a>
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -78,7 +78,7 @@ export default function Home() {
             height={16}
           />
           Examples
-        </a>
+        </a> */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
